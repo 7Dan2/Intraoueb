82b750e5fb3cab31d94aa457b07062d988c6f0c8
7Dan2 <37207556+7Dan2@users.noreply.github.com>
2018-05-14 09:31:37 +0200

    Add files via upload

---
+++ dateS.js
@@ -0,0 +1,6 @@
+var d = new Date();
+var y = d.getFullYear();
+var m = ["janvier",  "f&eacute;vrier",  "mars",  "avril", "mai", "juin", "juillet", "ao&ucirc;t", "septembre", "octobre", "novembre", "d&eacute;cembre"];
+var j = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+var day = d.getDate();
+document.getElementById("date").innerHTML = j[d.getDay()]+"&nbsp;"+day+"&nbsp;"+m[d.getMonth()]+"&nbsp;"+y;
